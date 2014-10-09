ebd7f64802a6d6adc0e00de47cfbd649b7bd6116
jesusVMayor <jesus@pexego.es>
2014-10-09 10:01:36 +0200

    [ADD]customer_product_name: Permite definir un nombre de producto para un cliente.

---
+++ project-addons/customer_product_name/stock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Pexego All Rights Reserved
+#    $Jesús Ventosinos Mayor <jesus@pexego.es>$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class stock_move(models.Model):
+
+    _inherit = 'stock.move'
+
+    # Se añade el campo para utilizarlo en el reporte qweb.
+    product_name = fields.Char('Product name', size=64,
+                               compute='_get_product_name', store=True)
+
+    @api.depends('product_id', 'partner_id')
+    def _get_product_name(self):
+        if not self.product_id:
+            self.product_name = ''
+        else:
+            customer_names = self.env['product.customer'].search(
+                [('product_id', '=', self.product_id.product_tmpl_id.id),
+                 ('customer_id', '=', self.partner_id.id)])
+            if customer_names:
+                self.product_name = customer_names[0].name
+            else:
+                self.product_name = self.product_id.name
