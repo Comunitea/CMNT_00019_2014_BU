ce907daa0d3c4f66c15e98f2f420c33cb76c910c
jesusVMayor <jesus@comunitea.com>
2017-08-28 12:22:20 +0200

    [FIX]bu_custom: Corregido filtro pedidos facturados.

--- project-addons/bu_custom/purchase.py
+++ project-addons/bu_custom/purchase.py
@@ -9,7 +9,17 @@ class PurchaseOrder(models.Model):
 
     _inherit = 'purchase.order'
 
+    invoiced = fields.Boolean(
+        'Invoice Received', copy=False,
+        help="It indicates that an invoice has been validated",
+        compute='_compute_invoiced')
+
     @api.multi
     def set_invoiced(self):
         self.order_line.write({'invoiced': True})
         self.step_workflow()
+
+    @api.depends('order_line.invoiced', 'order_line.state')
+    def _compute_invoiced(self):
+        for purchase in self:
+            purchase.invoiced = all(line.invoiced for line in purchase.order_line if line.state != 'cancel')
