a3b5f65710b63512305aca9e55e73b8f4bed3761
Omar (Comunitea) <omar@comunitea.com>
2020-04-20 17:29:50 +0200

    [UPD] global: Cambios menosrss para conseguir el opneupgrade de la 11.0

--- project-addons/account_group_move_lines/__init__.py
+++ project-addons/account_group_move_lines/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import invoice
-import account
+from . import invoice
+from . import account
